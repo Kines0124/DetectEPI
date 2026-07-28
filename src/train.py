5ceb3ba560146e38aafa1be393364c85147f0295
unknown <guilhermealvarenga0124@gmail.com>
2026-07-28 20:31:37 -0300

    Refatorada a classe de train, e adicionado a opção de treinar o modelo nano/small

--- src/train.py
+++ src/train.py
@@ -1,19 +1,23 @@
 from ultralytics import YOLO
 
-model = YOLO("yolov8n.pt")
-
-def main():
+def train_model(model_size ="n", epochs = 100):
+    model = YOLO(f"yolov8{model_size}.pt")
+    
     results = model.train(
         data="data/data.yaml",
-        epochs=100, ## Numero de  vezes que o modelo ve o dataset completo // Nos testes um valor entre 60-70 é o ideal, o valor 100 tem um leve overfitting
+        epochs=epochs, ## Numero de  vezes que o modelo ve o dataset completo // Nos testes um valor entre 60-70 é o ideal, o valor 100 tem um leve overfitting
         imgsz=640, ## Tamanhodas imagens
-        batch=32,  ## Quantidade de imagens por vez
+        batch=-1,  ## Quantidade de imagens por vez: -1 para autobatch -> Decide a quatidade sozinho
         workers=8, 
         device=0,  ## Definindo pra usar GPU
         patience=20,
         project="C:/Projetos/DetectEPI/runs",
-        name="detectepi_v2_classweights"
+        name=f"detectepi_{model_size}_epochs{epochs}"
     )
+    
+def main ():
+    train_model(model_size="s", epochs=100)
+    ## train_model(model_size="n", epochs=100)
 
 if __name__ == "__main__":
     main()
